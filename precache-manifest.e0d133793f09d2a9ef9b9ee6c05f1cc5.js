827a2ce78e3aa6d2a3f81183793f72157215b225
Perry Wang <perry.wzm@gmail.com>
2019-05-15 13:47:30 +0800

    Updates

---
+++ precache-manifest.e0d133793f09d2a9ef9b9ee6c05f1cc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fc3d38ed24b3676245d53c495104082",
+    "url": "/projects/index.html"
+  },
+  {
+    "revision": "03b5ce325a13d3c0930a",
+    "url": "/projects/static/css/main.5dec74c7.chunk.css"
+  },
+  {
+    "revision": "99f4d6b6632f79e61bd9",
+    "url": "/projects/static/js/2.a51a861a.chunk.js"
+  },
+  {
+    "revision": "03b5ce325a13d3c0930a",
+    "url": "/projects/static/js/main.6dac421f.chunk.js"
+  },
+  {
+    "revision": "766ba04493a904f18648",
+    "url": "/projects/static/js/runtime~main.aba5d133.js"
+  }
+]);
